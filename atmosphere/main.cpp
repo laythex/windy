bad5f1df0dd1a6580aa19ea18db18f7e6756b2fd
unknown <1204kosta@gmail.com>
2024-04-01 02:38:28 +0300

    Добавил сжимаемость атмосферы

--- atmosphere/main.cpp
+++ atmosphere/main.cpp
@@ -15,6 +15,10 @@ const double rad = 0.5;
 const double height = 0.5;
 // Коэффициент силы притяжения
 const double G = 0.00001;
+// Коэффициент плотности в экспоненте
+const double K = 10;
+// Плотность на поверхности планеты
+const double rho_0 = 1;
 
 // Вектор вращения планеты
 std::vector<double> w = {0, 0, 1};
@@ -55,6 +59,25 @@ public:
     }
 };
 
+class Density: public Expression
+{
+public:
+
+    void eval(Array<double> &values, const Array<double> &coord) const
+    {
+        double x = coord[0];
+        double y = coord[1];
+        double z = coord[2];
+        double r = sqrt(x * x + y * y + z * z);
+        if (r >= rad - DOLFIN_EPS){
+            values[0] = rho_0 * exp(-K*(r-rad));
+        }
+        else{
+            values[0] = 0;
+        }
+    }
+};
+
 // Граница поверхности
 
 class SurfaceBoundary : public SubDomain
@@ -83,7 +106,7 @@ class SpaceBoundary : public SubDomain
 
 int main()
 {
-    unsigned resolution = 25;
+    unsigned resolution = 30;
     auto universe = mshr::Sphere(Point(0, 0, 0), rad + height);
     auto planet = mshr::Sphere(Point(0, 0, 0), rad);
     auto atmosphere = universe - planet;
@@ -93,7 +116,7 @@ int main()
     auto Q = std::make_shared<PressureUpdate::FunctionSpace>(mesh);
 
     double dt = 0.01;
-    double T = 1;
+    double T = 2;
 
     // Давление на поверхности 1
     auto sur_p = std::make_shared<Constant>(1.0);
@@ -116,6 +139,7 @@ int main()
 
     auto k = std::make_shared<Constant>(dt);
     auto f = std::make_shared<GravityForces>();
+    auto rho = std::make_shared<Density>();
 
     TentativeVelocity::BilinearForm a1(V, V);
     TentativeVelocity::LinearForm L1(V);
@@ -125,16 +149,21 @@ int main()
     VelocityUpdate::LinearForm L3(V);
 
     a1.k = k;
+    a1.rho = rho;
     L1.k = k;
     L1.u0 = u0;
     L1.f = f;
+    L1.rho = rho;
     L2.k = k;
     L2.u1 = u1;
+    L2.rho = rho;
+    a3.rho = rho;
     L3.k = k;
     L3.u1 = u1;
     L3.p1 = p1;
+    L3.rho = rho;
 
-    // че-то с матрицами крутится для каждой вариационной задачи
+    // Решаем вариационные линейные задачи 
     Matrix A1, A2, A3;
     assemble(A1, a1);
     assemble(A2, a2);
@@ -149,8 +178,11 @@ int main()
 
     File ufile("results/velocity.pvd");
     File pfile("results/pressure.pvd");
+    //File dfile("results/density.pvd");
 
     double t = dt;
+
+    //dfile << *rho;
 
     while (t < T + DOLFIN_EPS)
     {
